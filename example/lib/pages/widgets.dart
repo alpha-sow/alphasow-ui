2fb59624ab55fbd155b4c00a30e0ab26acfe55d4
alphasow <alphaibrahima.sow@outlook.fr>
2025-08-01 16:12:58 +0200

    feat: refactor widget structure by moving to pages and removing unused widgets

---
+++ example/lib/pages/widgets.dart
@@ -0,0 +1,7 @@
+export 'banner.dart';
+export 'button.dart';
+export 'input.dart';
+export 'label.dart';
+export 'list_tile_ui.dart';
+export 'loader.dart';
+export 'show_alert_dialog.dart';
--- example/lib/widgets/widgets.dart
+++
@@ -1,7 +0,0 @@
-export 'banner_widget.dart';
-export 'button_widget.dart';
-export 'input_widget.dart';
-export 'label_widget.dart';
-export 'list_tile_ui_widget.dart';
-export 'loader_widget.dart';
-export 'show_alert_dailog.dart';
